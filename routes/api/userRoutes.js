c9dd9751c96598c2d339a48e15c15a835e967d02
Kevin <khampkevin@gmail.com>
2023-06-03 23:08:15 -0400

    userRoutes and controllers

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,6 +2,19 @@ const router = require('express').Router()
 
 const {
     getUsers,
-    getSingleUser,
+    getOneUser,
     createUser,
-} = 
+    deleteUser,
+    updateUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userController')
+
+// /api/users
+router.route('/').get(getUsers).post(createUser)
+
+// /api/users/:userId
+router.route('/:userId').get(getOneUser).delete(deleteUser).put(updateUser)
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend)
